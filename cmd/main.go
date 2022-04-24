2966e7d1577755f0acaf3063c21411fb8b3a7ec2
lodgvideon <lodgvideon@gmail.com>
2022-04-24 18:29:39 +0300

    Initial Commit

---
+++ cmd/main.go
@@ -0,0 +1,178 @@
+package main
+
+// import some pandora stuff
+// and stuff you need for your scenario
+// and protobuf contracts for your grpc service
+
+import (
+	"context"
+	"github.com/segmentio/kafka-go"
+	"github.com/segmentio/kafka-go/protocol"
+	"github.com/spf13/afero"
+	"github.com/yandex/pandora/cli"
+	"github.com/yandex/pandora/components/phttp/import"
+	"github.com/yandex/pandora/core"
+	"github.com/yandex/pandora/core/aggregator/netsample"
+	"github.com/yandex/pandora/core/import"
+	"github.com/yandex/pandora/core/register"
+	"os"
+	"strings"
+	"time"
+)
+
+type Ammo struct {
+	Topic   string `json:"topic,omitempty"`
+	Key     string `json:"key,omitempty"`
+	Tag     string `json:"tag,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
+type GunConfig struct {
+	KafkaBrokers           string `validate:"required" json:"kafka_brokers"` // localhost:9094,localhost:9094, will fail, without target defined
+	Compression            int
+	BatchSize              int
+	MaxAttempts            int
+	BatchBytes             int64
+	BatchTimeout           string
+	Balancer               string
+	WriteTimeout           string
+	Async                  bool
+	AllowAutoTopicCreation bool
+	RequiredAcks           int            `validate:"required"`
+	Headers                []HeaderConfig `validate:"required"` // Configuration will fail, without target defined // 1048576
+}
+type HeaderConfig struct {
+	Key   string `validate:"required"`
+	Value string `validate:"required"`
+}
+
+type Gun struct {
+	// Configured on construction.
+	client *kafka.Writer
+	conf   GunConfig
+	// Configured on Bind, before shooting
+	aggr core.Aggregator // May be your custom Aggregator.
+	core.GunDeps
+	ShootId string
+}
+
+func NewGun(conf GunConfig) *Gun {
+	return &Gun{conf: conf}
+}
+
+func (g *Gun) Bind(aggr core.Aggregator, deps core.GunDeps) error {
+	g.ShootId = os.Getenv("SHOOT_ID")
+	// create gRPC stub at gun initialization
+	brokers := g.conf.KafkaBrokers
+	brokersSlice := strings.Split(brokers, ",")
+	batchTimeOut, err := time.ParseDuration(g.conf.BatchTimeout)
+	if err != nil {
+		g.Log.Info("Duration was not set, accepted types are: 1s,1h,100ms")
+		batchTimeOut, _ = time.ParseDuration("1s")
+	}
+
+	writeTimeout, err := time.ParseDuration(g.conf.WriteTimeout)
+	if err != nil {
+		g.Log.Info("Duration was not set, accepted types are: 1s,1h,100ms")
+		writeTimeout, _ = time.ParseDuration("10s")
+	}
+	var balancer kafka.Balancer
+	switch g.conf.Balancer {
+	case "roundrobin":
+		balancer = &kafka.RoundRobin{}
+	case "crc32":
+		balancer = &kafka.CRC32Balancer{}
+	case "sarama":
+		balancer = &kafka.Hash{}
+	case "murmur2":
+		balancer = &kafka.Murmur2Balancer{}
+	case "leastbytes":
+		balancer = &kafka.LeastBytes{}
+	}
+
+	g.client = &kafka.Writer{
+		Addr: kafka.TCP(brokersSlice...),
+		// NOTE: When Topic is not defined here, each Message must define it instead.
+		Balancer:               balancer,
+		MaxAttempts:            g.conf.MaxAttempts,
+		BatchSize:              g.conf.BatchSize,
+		BatchBytes:             g.conf.BatchBytes,
+		BatchTimeout:           batchTimeOut,
+		WriteTimeout:           writeTimeout,
+		RequiredAcks:           kafka.RequiredAcks(g.conf.RequiredAcks),
+		Async:                  g.conf.Async,
+		Compression:            kafka.Compression(g.conf.Compression),
+		Logger:                 kafka.LoggerFunc(g.logf),
+		ErrorLogger:            kafka.LoggerFunc(g.logError),
+		AllowAutoTopicCreation: g.conf.AllowAutoTopicCreation,
+	}
+	g.aggr = aggr
+	g.GunDeps = deps
+	return nil
+}
+func (g *Gun) logf(msg string, a ...interface{}) {
+	g.Log.Info(msg)
+}
+func (g *Gun) logError(msg string, a ...interface{}) {
+	g.Log.Error(msg)
+}
+
+func (g *Gun) Shoot(ammo core.Ammo) {
+	customAmmo := ammo.(*Ammo)
+	g.shoot(customAmmo)
+}
+
+func (g *Gun) shoot(ammo *Ammo) {
+	sample := netsample.Acquire(ammo.Topic + "_" + ammo.Tag)
+	defer func() {
+		g.aggr.Report(sample)
+	}()
+
+	var headers []kafka.Header
+	headers = append(headers, protocol.Header{Key: "SHOOT_ID", Value: []byte(g.ShootId)})
+	for _, header := range g.conf.Headers {
+		headers = append(headers, protocol.Header{Key: header.Key, Value: []byte(header.Value)})
+	}
+
+	err := g.client.WriteMessages(context.Background(),
+		kafka.Message{
+			Topic: ammo.Topic,
+			Key:   []byte(ammo.Key), Value: []byte(ammo.Message),
+			Headers: headers})
+	if err != nil {
+		sample.SetProtoCode(500)
+		return
+	}
+	sample.SetProtoCode(200)
+
+}
+
+func main() {
+	//debug.SetGCPercent(-1)
+	// Standard imports.
+	fs := afero.NewOsFs()
+	coreimport.Import(fs)
+	// May not be imported, if you don't need http guns and etc.
+	phttp.Import(fs)
+
+	// Custom imports. Integrate your custom types into configuration system.
+	coreimport.RegisterCustomJSONProvider("kafka_provider", func() core.Ammo { return &Ammo{} })
+
+	register.Gun("pandora_kafka_shooter", NewGun, func() GunConfig {
+		return GunConfig{
+			KafkaBrokers:           "localhost:9094",
+			Compression:            0,
+			BatchSize:              100,
+			MaxAttempts:            10,
+			BatchBytes:             1048576,
+			BatchTimeout:           "1s",
+			WriteTimeout:           "10s",
+			Balancer:               "sarama",
+			Async:                  false,
+			AllowAutoTopicCreation: false,
+			Headers:                []HeaderConfig{},
+		}
+	})
+
+	cli.Run()
+}
